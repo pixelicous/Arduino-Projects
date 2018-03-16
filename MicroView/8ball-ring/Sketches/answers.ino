85f74b82797e56e5840afca713de9510dc4f1e42
pixelicous <pixel@PixeLabs-Box.local>
2018-03-16 22:56:23 +0200

    add answers function

---
+++ MicroView/8ball-ring/Sketches/answers.ino
@@ -0,0 +1,26 @@
+void answers() {
+  int randomNumber;
+  
+  randomSeed(analogRead(0));
+  randomNumber = random(1,5);
+
+  switch (randomNumber) {
+    case 1:
+
+    break;
+    case 2:
+
+    break;
+    case 3:
+
+    break;
+    case 4:
+
+    break;
+    case 5:
+    break;
+  }
+
+  
+}
+
