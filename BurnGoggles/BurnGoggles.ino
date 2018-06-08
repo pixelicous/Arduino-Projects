603e714d0bf7c75bfcfc9ed02a7d9a0061dc9484
pixelicous <pixel@PixeLabs-Box.local>
2018-06-08 13:58:16 +0300

    Rename folder and split files

--- BurnGoggles/BurnGoggles.ino
+++ BurnGoggles/BurnGoggles.ino
@@ -1,7 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 #include <EEPROM.h>
 #include "prng.h"
-
+#ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
+ #include <avr/power.h>
+#endif
 /* EEPROM usage:
  *   byte 0: last used mode
  *   byte 1: PRNG seed
@@ -22,9 +24,9 @@ extern uint16_t prng_register;
 void setup() {
   // put your setup code here, to run once:
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-//  #if defined (__AVR_ATtiny85__)
-//    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-//  #endif
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
 
   
   strip.begin();
@@ -58,27 +60,7 @@ void loop() {
   }
 }
 
-byte pickPrimary(byte oldPrimary, byte otherOldPrimary) {
-  byte newPrimary=oldPrimary;
-  while(newPrimary == oldPrimary || newPrimary == otherOldPrimary) {
-    // using this random number generator is
-    // pretty expensive - about 692 bytes.
-    // newPrimary = random(0,6);
-    newPrimary = get3BitRandomLessThan(6);
-  }
-  return newPrimary;
-}
 
-uint32_t primaryToColour(byte primary) {
-  switch(primary) {
-    case 0: return strip.Color(  0,  0,255);
-    case 1: return strip.Color(  0,255,  0);
-    case 2: return strip.Color(  0,128,64);
-    case 3: return strip.Color(255,  0,  0);
-    case 4: return strip.Color(192,  0,192);
-    case 5: return strip.Color(255,128,  0);
-  }
-}
 
 void loop_slow_primary_fill() {
 
@@ -429,15 +411,4 @@ uint8_t twopow(uint8_t n) {
   return v;
 }
 
-
-void setPixelMirror(byte pix, uint32_t colour) {
-  strip.setPixelColor(pix, colour);
-  strip.setPixelColor(NUMLEDS - pix - 1, colour);
-}
-
-void setAllPixels(uint32_t colour) {
-  for(byte pix=0; pix<NUMLEDS; pix++) {
-    strip.setPixelColor(pix, colour);  
-  }
-}
 
---
+++ BurnGoggles/functions.ino
@@ -0,0 +1,33 @@
+byte pickPrimary(byte oldPrimary, byte otherOldPrimary) {
+  byte newPrimary=oldPrimary;
+  while(newPrimary == oldPrimary || newPrimary == otherOldPrimary) {
+    // using this random number generator is
+    // pretty expensive - about 692 bytes.
+    // newPrimary = random(0,6);
+    newPrimary = get3BitRandomLessThan(6);
+  }
+  return newPrimary;
+}
+
+uint32_t primaryToColour(byte primary) {
+  switch(primary) {
+    case 0: return strip.Color(  0,  0,255);
+    case 1: return strip.Color(  0,255,  0);
+    case 2: return strip.Color(  0,128,64);
+    case 3: return strip.Color(255,  0,  0);
+    case 4: return strip.Color(192,  0,192);
+    case 5: return strip.Color(255,128,  0);
+  }
+}
+
+void setPixelMirror(byte pix, uint32_t colour) {
+  strip.setPixelColor(pix, colour);
+  strip.setPixelColor(NUMLEDS - pix - 1, colour);
+}
+
+void setAllPixels(uint32_t colour) {
+  for(byte pix=0; pix<NUMLEDS; pix++) {
+    strip.setPixelColor(pix, colour);  
+  }
+}
+
