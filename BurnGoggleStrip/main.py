a8ea6516543baf7bcf826e158aba21b55375e05b
Pix <pixi@pixelabs.com>
2018-08-13 01:53:42 +0300

    GogglesStrp - new modes, remove white led setup and from colors

--- BurnGoggleStrip/main.py
+++ BurnGoggleStrip/main.py
@@ -15,29 +15,56 @@ num_pixels = 8
 
 BPP=4
 
-pixels = neopixel.NeoPixel(pixpin, num_pixels, bpp=BPP, brightness=0.01, auto_write=False)
+pixels = neopixel.NeoPixel(pixpin, num_pixels, brightness=0.02, auto_write=False)
 
 #Demos to run
-sparks = 1
-whiteSparkles = 1
-firePlace = 1
-rainbowDemo = 1
-rainbowCycleDemo = 1
+knightrider = 1
+sparks = 0
+whiteSparkles = 0
+firePlace = 0
+rainbowDemo = 0
+rainbowCycleDemo = 0
 
 # Constants
-RED = (255, 0, 0, 0)
-YELLOW = (255, 150, 0, 0)
-ORANGE = (255, 40, 0, 0)
-GREEN = (0, 255, 0, 0)
-TEAL = (0, 255, 120, 0)
-CYAN = (0, 255, 255, 0)
-BLUE = (0, 0, 255, 255, 0)
-PURPLE = (180, 0, 255, 0)
-MAGENTA = (255, 0, 20, 0)
-WHITE = (255, 255, 255, 0)
-BLACK = (0, 0, 0, 0)
+RED = (255, 0, 0)
+DARKRED = (70, 0, 0)
+YELLOW = (255, 150, 0)
+ORANGE = (255, 40, 0)
+GREEN = (0, 255, 0)
+TEAL = (0, 255, 120)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+MAGENTA = (255, 0, 20)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 colorsList = [RED,YELLOW,ORANGE,GREEN,TEAL,CYAN,BLUE,PURPLE,MAGENTA,WHITE]
+
+def knightriderDemo():
+    for i in range(num_pixels):
+        pixels[i] = DARKRED 
+    pixels.show()
+    time.sleep(0.9)
+    for i in range(num_pixels/2):
+        pixels[i] = RED
+        test = num_pixels-i-1
+        pixels[test] = RED
+        time.sleep(0.03)
+        pixels.show()
+    for i in range(num_pixels/2):
+        pixels[i] = CYAN
+        test = num_pixels-i-1
+        pixels[test] = PURPLE
+        time.sleep(0.3)
+        pixels.show()
+    for i in range(num_pixels/2):
+        pixels[i] = GREEN
+        test = num_pixels-i-1
+        pixels[test] = BLUE
+        time.sleep(0.3)
+        pixels.show()
+    pixels.show()
 
 def whiteSparklesDemo(wait,iterate):
     for i in range(iterate):
@@ -63,7 +90,7 @@ def sparksDemo(wait,iterate):
 def firePlaceDemo(wait,iterate):
     for i in range(iterate):
         pixel = random.randint(0, num_pixels-1)
-        color = (random.randint(50, 255), random.randint(0, 40), 0, 0)
+        color = (random.randint(50, 255), random.randint(0, 40), 0)
         pixels[pixel] = color
         pixels.write()
         time.sleep(wait)
@@ -91,15 +118,17 @@ def shiftList(list, amount):
 
 def wheel(pos):
     if pos < 85:
-        return (int(pos * 3), int(255 - (pos * 3)), 0, 0)
+        return (int(pos * 3), int(255 - (pos * 3)), 0)
     elif pos < 170:
         pos -= 85
-        return (int(255 - (pos * 3)), 0, int(pos * 3), 0)
+        return (int(255 - (pos * 3)), 0, int(pos * 3))
     else:
         pos -= 170
-        return (0, int(pos * 3), int(255 - pos * 3), 0)
+        return (0, int(pos * 3), int(255 - pos * 3))
 
 while True:
+    if knightrider:
+        knightriderDemo()
 
     if whiteSparkles:
         whiteSparklesDemo(0.001,300)
@@ -117,4 +146,3 @@ while True:
     if rainbowCycleDemo:
         for i in range(3):
             rainbow_cycle(.002)
-
