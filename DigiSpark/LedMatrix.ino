02de06d8e7be08ec76ee8d51737a081a3bb96a85
Pix <pixi@pixelabs.com>
2018-08-22 01:35:33 +0300

    update anim

--- DigiSpark/LedMatrix.ino
+++ DigiSpark/LedMatrix.ino
@@ -6,15 +6,11 @@ const int CLK_PIN = 0;
 
 const uint64_t IMAGES[] = {
   0x3c7ec3bdffdb7e3c,
-  0xc3813c42002481c3,
   0x1824428181819966,
   0x183c7e8181819966,
   0x183c7effff819966,
   0x183c7effffffff66,
-  0xe7c3810000000099,
-  0x183c7effffffff66,
   0x182442ff9999ff66,
-  0x182442bdff999966,
   0x04649c185e380c0c,
   0x2818183838180c0c,
   0x04649c185e380c0c,
@@ -28,29 +24,17 @@ const uint64_t IMAGES[] = {
   0xc6633118183163c6,
   0x31188cc6c68c1831,
   0x188cc66363c68c18,
-  0x008cc66363c68c00,
-  0x00300000c6c60000,
-  0x003c420066660000,
-  0x003c400006660000,
-  0x003c420000660000,
-  0x0018180066660000,
-  0x1800666600000000,
-  0x6666000000000000,
-  0xffff000000000000,
-  0xffef250400000000,
-  0xffffbab220200000,
-  0xfffffffba9212000,
-  0xfffffffff7f7d5d4,
-  0xffffff7d74541400,
-  0xfffffffda9a98080,
-  0xfffff7d756020200,
-  0xffffffffffebab89,
-  0xffffffffffffffff,
+  0x1c36363030307800,
+  0x3c66666666663c00,
+  0x7c66666666666600,
+  0x1c36363030307800,
+  0x3c66666666663c00,
+  0x7c66666666666600,
+  0x8142242424425a99,
+  0x4242242424245a5a,
   0xc19999c19999c1ff,
   0x99c9e1c19999c1ff,
   0xc39999f99999c3ff,
-  0xc3a164607050b1c3,
-  0x3c5e9b9f8faf4e3c
 };
 const int IMAGES_LEN = sizeof(IMAGES)/8;
 
@@ -74,9 +58,11 @@ void displayImage(uint64_t image) {
 }
 int i=0;
 void loop() {
-  displayImage(IMAGES[i]);
-  if (++i >= IMAGES_LEN) {
-    i = 0;
+  int time = random(200,800);
+
+  for(i=0;i<IMAGES_LEN;i++) {
+    displayImage(IMAGES[i]);
+    delay(time );
   }
-  delay(00);
+
 }
